d6aa7d289923abfdcf71c592d538c3ea42c11940
ugbeadie <ugbeadie3@gmail.com>
2022-05-21 09:01:39 +0100

    added title and  comments

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const currentDay = document.querySelector('.day');
 const searchedCity = document.querySelector('.city');
 const tempValue = document.querySelector('.temp_value');
 const tempDesc = document.querySelector('.temp_desc');
+const title = document.querySelector('.title');
 const api = {
     key: '9f3194b93838e97cb74460234c6782c9',
     url: 'https://api.openweathermap.org/data/2.5/weather'
 }
+
+// GET CURRENT DAY
 
 function today(d) {
     let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 
@@ -18,17 +21,23 @@ function today(d) {
     return `${today}`;
 }
 
+// EVENT HANDLERS
+
 searchBtn.addEventListener('click', () => {
-    getWeather(searchBox.value)
+    getWeather(searchBox.value);
 })
 
 searchBox.addEventListener('keypress', setQuery);
+
+// SETQUERY FUNCTION EXECUTED
 
 function setQuery(evt) {
     if (evt.keyCode == 13) {
         getWeather(searchBox.value);
     }
 }
+
+// API GET REQUEST FROM THE SERVER
 
 function getWeather(query) {
     fetch(`${api.url}?q=${query}&units=metric&appid=${api.key}`)
@@ -39,13 +48,9 @@ function getWeather(query) {
     })
     .then(data => {
         let now = new Date();
-        currentDay.innerHTML = today(now)
 
+        currentDay.innerHTML = today(now);
         searchedCity.innerHTML = `${data.name}, ${data.sys.country}`
-        // if(searchedCity !== data.name) {
-        //     alert('not a city');
-        // }
-
         tempValue.innerHTML = `${Math.round(data.main.temp)}<span>°C</span>`;
         tempDesc.innerHTML = `${data.weather[0].description}`
     })
